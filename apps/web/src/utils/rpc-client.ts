1cafc274fd7197ccf4c33ac25036836ea6d8323f
Linruoxin-Sy <342972612@qq.com>
2026-05-22 23:38:06 +0800

    refactor(web): external hook

--- apps/web/src/utils/rpc-client.ts
+++ apps/web/src/utils/rpc-client.ts
@@ -2,31 +2,34 @@ import type { AppType } from '@tutorhub/api/rpc';
 import { hc } from 'hono/client';
 import ky, { type AfterResponseHook, type BeforeRequestHook } from 'ky';
 
+const beforeRequestHooks: BeforeRequestHook[] = [
+  // ({ request }) => {
+  //   // 显式添加 options 参数
+  //   const token = localStorage.getItem('token');
+  //   if (token) {
+  //     // 在 ky 的 BeforeRequestHook 中，request 是标准的 Request 对象
+  //     request.headers.set('Authorization', `Bearer ${token}`);
+  //   }
+  // },
+];
+
+const afterResponseHooks: AfterResponseHook[] = [
+  // async ({ response }) => {
+  // if (!response.ok) {
+  //   const errorData = await response.json().catch(() => ({}));
+  //   console.error('API Error:', errorData);
+  //   if (response.status === 401) {
+  //     window.location.href = '/login';
+  //   }
+  // }
+  // },
+];
+
 const kyInstance = ky.create({
   timeout: 10000,
   hooks: {
-    beforeRequest: [
-      // ({ request }) => {
-      //   // 显式添加 options 参数
-      //   const token = localStorage.getItem('token');
-      //   if (token) {
-      //     // 在 ky 的 BeforeRequestHook 中，request 是标准的 Request 对象
-      //     request.headers.set('Authorization', `Bearer ${token}`);
-      //   }
-      // },
-    ] satisfies BeforeRequestHook[],
-
-    afterResponse: [
-      // async ({ response }) => {
-      // if (!response.ok) {
-      //   const errorData = await response.json().catch(() => ({}));
-      //   console.error('API Error:', errorData);
-      //   if (response.status === 401) {
-      //     window.location.href = '/login';
-      //   }
-      // }
-      // },
-    ] satisfies AfterResponseHook[],
+    beforeRequest: beforeRequestHooks,
+    afterResponse: afterResponseHooks,
   },
 });
 
