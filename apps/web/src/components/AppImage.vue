f48424d15a67bf5fc6024befed7270c04a7462a4
Linruoxin-Sy <342972612@qq.com>
2026-05-24 00:43:55 +0800

    chore(web): add more vite plugin for development

---
+++ apps/web/src/components/AppImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="src" :alt="alt" class="h-full w-full bg-cover bg-no-repeat bg-center object-cover" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  src: string;
+  alt?: string;
+}>();
+
+const src = computed(() => {
+  if (props.src.startsWith('http') || props.src.startsWith('data:')) {
+    return props.src;
+  } else {
+    return import.meta.env.VITE_ASSETS_URL + props.src;
+  }
+});
+</script>
