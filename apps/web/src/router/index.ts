db62bb737229b3ee3239e780b759ad21dfa21777
Linruoxin-Sy <342972612@qq.com>
2026-05-28 17:07:07 +0800

    feat(web): remove environment variable from router history

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,9 +1,8 @@
-import { getEnv } from '@/utils';
 import { createRouter, createWebHistory } from 'vue-router';
 import { handleHotUpdate, routes } from 'vue-router/auto-routes';
 
 const router = createRouter({
-  history: createWebHistory(getEnv('BASE_URL')),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
