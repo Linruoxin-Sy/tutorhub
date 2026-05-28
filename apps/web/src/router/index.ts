3e1e5ba1dbefeab2b6e7fb778867ae5756cdb1c3
Linruoxin-Sy <342972612@qq.com>
2026-05-28 16:25:23 +0800

    feat(web): change env

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,8 +1,9 @@
+import { getEnv } from '@/utils';
 import { createRouter, createWebHistory } from 'vue-router';
 import { handleHotUpdate, routes } from 'vue-router/auto-routes';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(getEnv('BASE_URL')),
   routes: [
     {
       path: '/',
