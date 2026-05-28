f0a89426bf42ab8872021468e1833cec9d470688
Linruoxin-Sy <342972612@qq.com>
2026-05-28 15:44:37 +0800

    refactor(web): style change

--- apps/web/src/pages/dashboard.vue
+++ apps/web/src/pages/dashboard.vue
@@ -2,12 +2,8 @@
   <main class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
     <div class="space-y-6">
       <StatsSection />
-
-      <section class="space-y-6">
-        <AssistantSection />
-
-        <RecentSessionsSection />
-      </section>
+      <AssistantSection />
+      <RecentSessionsSection />
     </div>
   </main>
 </template>
