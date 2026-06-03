127588a09e80951fcc3bf9ed8e178b957032d00f
Linruoxin-Sy <342972612@qq.com>
2026-06-04 02:22:52 +0800

    feat(web): complete user logout

--- apps/web/src/layouts/AppAvatar.vue
+++ apps/web/src/layouts/AppAvatar.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="size-10 rounded-full overflow-hidden bg-gray-100 dark:bg-[#202020] cursor-pointer">
-    <!-- img placeholder -->
-    <div class="flex size-full items-center justify-center"></div>
-    <!-- img -->
-    <AppImage src="" />
+  <div class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+      <div class="w-10 rounded-full">
+        <!-- img placeholder -->
+        <div class="flex size-full items-center justify-center bg-gray-100 dark:bg-[#202020]"></div>
+        <!-- img -->
+        <AppImage src="" />
+      </div>
+    </div>
+    <ul
+      tabindex="-1"
+      class="menu menu-sm dropdown-content bg-gray-100 dark:bg-[#202020] rounded-box z-1 mt-3 w-52 p-2 shadow"
+    >
+      <li @click="logout">
+        <a class="text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+          >Logout</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/features/auth/stores/user';
+
+const router = useRouter();
+const userStore = useUserStore();
+
+const logout = () => {
+  userStore.logout();
+  router.push('/login');
+};
+</script>
