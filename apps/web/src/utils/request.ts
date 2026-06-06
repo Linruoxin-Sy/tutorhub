bd31bc3bec767bb18716a6830c735608f0d8f8bd
Linruoxin-Sy <342972612@qq.com>
2026-06-06 23:41:33 +0800

    feat(web): add interceptors in the axios instance

--- apps/web/src/utils/request.ts
+++ apps/web/src/utils/request.ts
@@ -1,7 +1,27 @@
 import axios from 'axios';
 import { getEnv } from './env';
+import router from '@/router';
 
 export const request = axios.create({
   baseURL: getEnv('BASE_URL'),
   timeout: 5000,
 });
+
+request.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+request.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      localStorage.removeItem('token');
+      router.push('/login');
+    }
+    return Promise.reject(error);
+  },
+);
