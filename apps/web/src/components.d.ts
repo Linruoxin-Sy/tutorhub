aa14f78793cb6275c3334f3caa3e10cc96b96c2f
Linruoxin-Sy <342972612@qq.com>
2026-06-08 01:07:29 +0800

    style: code style format

--- apps/web/src/components.d.ts
+++ apps/web/src/components.d.ts
@@ -11,7 +11,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AppImage: typeof import('./components/AppImage.vue')['default']
     ConfirmDialog: typeof import('./components/ConfirmDialog.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
